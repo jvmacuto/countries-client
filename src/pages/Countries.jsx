065c97cacc2c7cf0662e5b2f340ec6ee4e197f33
John Vincent <macutojohnvincent@gmail.com>
2024-06-15 09:59:09 +1200

    updated countries jsx

--- src/pages/Countries.jsx
+++ src/pages/Countries.jsx
@@ -1,5 +1,41 @@
+import { useQuery$ } from "@preact-signals/query";
+import { useEffect, useState } from "preact/hooks";
+import { Suspense } from "preact/compat";
+
 const Countries = () => {
-  return <>Countries</>;
+  const fetchCountries = async () => {
+    const results = await fetch("https://jm-countries-api.azurewebsites.net");
+
+    const resultsJson = results.json();
+    console.log("fetch", resultsJson);
+
+    return resultsJson;
+  };
+
+  const { data, isLoading, error } = useQuery$(() => ({
+    queryKey: ["key"],
+    queryFn: fetchCountries,
+    //suspense: true,
+  }));
+
+  console.log("Data", data);
+  return (
+    <>
+      <h1>Countries</h1>
+
+      {isLoading ? (
+        <b> Loading .. </b>
+      ) : error ? (
+        <b>There's an error: {error.message}</b>
+      ) : data ? (
+        <ul>
+          {data.map((country) => (
+            <li key={country.Code}>{country.Name}</li>
+          ))}
+        </ul>
+      ) : null}
+    </>
+  );
 };
 
 export default Countries;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [preact()],
-})
+  resolve: {
+    //add this line
+    dedupe: ["preact"],
+    alias: [
+      { find: "react", replacement: "preact/compat" },
+      { find: "react-dom", replacement: "preact/compat" },
+      { find: "@preact/signals-react", replacement: "@preact/signals" },
+    ],
+  },
+});
